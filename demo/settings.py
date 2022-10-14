fef2800bbead52d252b9a5e081495708b93ca664
brohui <hui.dev@outlook.com>
2022-10-14 11:02:31 +0800

    blank page

--- demo/settings.py
+++ demo/settings.py
@@ -127,3 +127,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'django_tabler_ng:sign_in'
+SITE_ID = 'django tabler demo site'
--- django_tabler_ng/urls.py
+++ django_tabler_ng/urls.py
@@ -8,5 +8,5 @@ app_name = 'django_tabler_ng'
 urlpatterns = [
     path('sign_in/', auth_views.LoginView.as_view(template_name='django_tabler_ng/signin.html'), name='sign_in'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('sign_out/', auth_views.LogoutView.as_view(template_name='django_tabler_ng/signout.html'), name='logout'),
+    path('sign_out/', auth_views.LogoutView.as_view(template_name='django_tabler_ng/signout.html'), name='sign_out'),
 ]
