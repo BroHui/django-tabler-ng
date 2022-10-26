bbb543d5609beba7f498012bd209d6920bb3aad9
brohui <hui.dev@outlook.com>
2022-10-26 14:21:11 +0800

    add blank page example

--- src/django_tabler_ng/bootstrap.py
+++ src/django_tabler_ng/bootstrap.py
@@ -3,16 +3,16 @@ from importlib import import_module
 from django.conf import settings
 
 BOOTSTRAP5_DEFAULTS = {
-    "css_url": {
-        "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-        "integrity": "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
-        "crossorigin": "anonymous",
-    },
-    "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
-        "integrity": "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
-        "crossorigin": "anonymous",
-    },
+    # "css_url": {
+    #     "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+    #     "integrity": "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
+    #     "crossorigin": "anonymous",
+    # },
+    # "javascript_url": {
+    #     "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+    #     "integrity": "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
+    #     "crossorigin": "anonymous",
+    # },
     "theme_url": None,
     "javascript_in_head": False,
     "use_i18n": False,
